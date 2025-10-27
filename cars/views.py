e19f610b836cdac788dfec94e2e8828e41f7b02f
TulioBreyner <marcopbreyner@gmail.com>
2025-10-27 11:52:47 -0300

    criando formulario para cadastro de novo carro

--- cars/views.py
+++ cars/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from cars.models import Car
+from cars.forms import CarForm
 
 def cars_view(request):
     cars = Car.objects.all().order_by('-model')
     search = request.GET.get('search')
     
     if search:
-        cars = Car.objects.filter(model__icontains=search).order_by('model')
+        cars = Car.objects.filter(model__icontains=search)
 
     return render(
         request,
         'cars.html',
         {'cars': cars}
     )
+
+def new_car_view(request):
+    new_car_form = CarForm()
+    return render(request, 'new_car.html', { 'new_car_form': new_car_form })
